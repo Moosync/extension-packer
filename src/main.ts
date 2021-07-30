255f47caec672839cd56460ca0556874448dcdfe
ovenoboyo <ovenoboyo@gmail.com>
2021-07-30 16:16:14 +0530

    Consider user input for include and exclude

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,17 @@
 import yargs from 'yargs'
 import { hideBin } from 'yargs/helpers'
 import path from 'path'
-import { generateZip, validateManifest } from './utils.js'
+import { generateZip, validateManifest, generateFileName } from './utils.js'
 
 const yarg = yargs(hideBin(process.argv))
 const args = yarg.command('path', 'Path of extension').alias('p', 'path').argv
 
 const extPath = path.resolve(process.cwd(), (await args)["path"] as string)
 
-const outputFile = await validateManifest(extPath)
-if (outputFile) {
-    await generateZip(extPath, outputFile)
+const manifest = await validateManifest(extPath)
+if (manifest) {
+    const outputFile = await generateFileName(extPath, manifest)
+    if (outputFile) {
+        await generateZip(extPath, outputFile, manifest.mopack)
+    }
 }
--- src/utils.ts
+++ src/utils.ts
@@ -2,35 +2,52 @@ import { promises as fsP, createWriteStream } from 'fs'
 import path from 'path'
 import archiver from 'archiver'
 
-type manifest = { extensionEntry?: string, moosyncExtension?: string, packageName?: string, name?: string, version?: string}
+type mopackOptions = { include?: string | string[], exclude?: string | string[] }
+type manifest = { extensionEntry?: string, moosyncExtension?: string, displayName?: string, name?: string, version?: string, mopack?: mopackOptions}
 
-export async function validateManifest(basePath: string): Promise<string> {
+export async function validateManifest(basePath: string): Promise<manifest> {
     const manifestPath = path.join(basePath, 'package.json')
     try {
         // check if package.json exists
         await fsP.access(manifestPath)
         const parsedManifest = await parseManifest(manifestPath)
         await fsP.access(parsedManifest.extensionEntry)
-        return generateFileName(basePath, parsedManifest)
+        return parsedManifest
     } catch (e) {
         console.error("Unable to parse package.json", (e as Error).message)
     }
-    return ''
+    return {}
 }
 
-function generateFileName(basePath: string, manifest: manifest) {
-    return path.join(basePath, `${manifest.packageName}-${manifest.version}.msox`)
+export function generateFileName(basePath: string, manifest: manifest) {
+    return path.join(basePath, `${manifest.name}-${manifest.version}.msox`)
 }
 
-export async function generateZip(basePath: string, target: string) {
+export async function generateZip(basePath: string, target: string, options?: mopackOptions) {
     await removeFileIfExists(target)
     var output = createWriteStream(target);
     const zip = archiver('zip', { store: true })
+    let ignore: string[] = []
+
+    if (options && options.exclude) {
+        ignore = (Array.isArray(options.exclude) ? options.exclude : [options.exclude])
+    }
+
     zip.pipe(output)
     zip.glob(`**/*`, {
         cwd: basePath,
-        ignore: ['node_modules/**', '.git/**', '*.lock', '*.zip', '*.msox']
+        ignore: ['node_modules/**', '.git/**', '*.lock', '*.zip', '*.msox', ...ignore]
     })
+
+    if (options && options.include) {
+        if (!Array.isArray(options.include)) {
+            options.include = [options.include]
+        }
+        for (const inc of options.include) {
+            zip.glob(inc, { cwd: basePath })
+        }
+    }
+
     await zip.finalize()
 }
 
@@ -42,7 +59,7 @@ async function removeFileIfExists(filePath: string) {
     }
 }
 
-function implementsTKeys<T>(obj: any, keys: (keyof T)[]): obj is T {
+function implementsTKeys<T>(obj: any, keys: string[]): obj is T {
     if (!obj || !Array.isArray(keys)) {
         return false;
     }
@@ -51,8 +68,8 @@ function implementsTKeys<T>(obj: any, keys: (keyof T)[]): obj is T {
 
 async function parseManifest(manifestPath: string): Promise<manifest> {
     const manifest = JSON.parse(await fsP.readFile(manifestPath, 'utf-8'))
-    if (implementsTKeys<manifest>(manifest, ['extensionEntry', 'moosyncExtension', 'name', 'packageName', 'version'])) {
+    if (implementsTKeys<manifest>(manifest, ['extensionEntry', 'moosyncExtension', 'name', 'displayName', 'version'])) {
         return manifest
     }
-    throw new Error('Manifest must include fields "extensionEntry", "moosyncExtension", "name", "packageName", "version"')
+    throw new Error('Manifest must include fields "extensionEntry", "moosyncExtension", "name", "displayName", "version"')
 }
