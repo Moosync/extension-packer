87fa21dac26a7d3572ae1d916efcb52761406743
ovenoboyo <ovenoboyo@gmail.com>
2021-10-30 20:00:12 +0530

    Upgrade deps

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,19 @@
 #!/usr/bin/env node
 
-import yargs from 'yargs'
-import { hideBin } from 'yargs/helpers'
-import path from 'path'
-import { generateZip, validateManifest, generateFileName } from './utils.js'
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+import path from "path";
+import { generateZip, validateManifest, generateFileName } from "./utils.js";
 
-const yarg = yargs(hideBin(process.argv))
-const args = yarg.command('path', 'Path of extension').alias('p', 'path').argv
+const yarg = yargs(hideBin(process.argv));
+const args = yarg.command("path", "Path of extension").alias("p", "path").argv;
 
-const extPath = path.resolve(process.cwd(), (await args)["path"] as string)
+const extPath = path.resolve(process.cwd(), (await args)["path"] as string);
 
-const manifest = await validateManifest(extPath)
+const manifest = await validateManifest(extPath);
 if (manifest) {
-    const outputFile = await generateFileName(extPath, manifest)
+    const outputFile = await generateFileName(extPath, manifest);
     if (outputFile) {
-        await generateZip(extPath, outputFile, manifest.mopack)
+        await generateZip(extPath, outputFile, manifest.mopack);
     }
 }
